37e3d633d5ec11d6737f25b3428c681fc6ee1ecc
Mustafa0801 <mustafahassancheema@gmail.com>
2026-09-15 11:19:07 +0500

    Add file

---
+++ LFR-test.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(4800);
+  pinMode(A0, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorReading = analogRead(irSensorPin);
+  Serial.write(sensorReading);
+}
