91726cb5240f975552957eea14f64ea10e8ec54e
Mustafa0801 <mustafahassancheema@gmail.com>
2026-09-17 21:04:39 +0500

    updated code and added comments

--- sensor-test-code/sensor-test.ino
+++ sensor-test-code/sensor-test.ino
@@ -1,7 +1,7 @@
-#define ir_L1 8 // left most sensor
-#define ir_L2 9
-#define ir_M 10 // middle sensor
-#define ir_R1 11
+#define ir_L2 8  // left most sensor
+#define ir_L1 9  // left middle sensor
+#define ir_M 10  // middle sensor
+#define ir_R1 11 // right middle sensor
 #define ir_R2 12 // right most sensor
 
 //  total 5 sensors in array
@@ -10,8 +10,8 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(ir_L1, INPUT);
   pinMode(ir_L2, INPUT);
+  pinMode(ir_L1, INPUT);
   pinMode(ir_M, INPUT);
   pinMode(ir_R1, INPUT);
   pinMode(ir_R2, INPUT);
@@ -19,10 +19,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  ReadSensor(ir_L1);
-  ReadSensor(ir_L2);
-  ReadSensor(ir_M);
-  ReadSensor(ir_R1);
-  ReadSensor(ir_R2);
-  Serial.println();
+  int linePos = ReadSensor(); // the readings will be printed and line position will be stored in linePos
+
+  Serial.print(" | linePos: "+String(linePos));   // example of one Serial line:  0 1 1 1 1  | linePos: -4 
+  Serial.println(); // goes to next line in Serial
+
+  // linePos will be used as condition to select correct movement of robot such that it follows the line
 }
