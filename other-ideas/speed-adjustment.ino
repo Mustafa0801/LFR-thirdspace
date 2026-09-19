ffd98b9a8dcef0656d3624791d4ea8872113708e
Mustafa0801 <mustafahassancheema@gmail.com>
2026-09-19 16:38:41 +0500

    update speed adjustment code
    added min and max thresholds for speed, also made it so that line position has greater chance to be 0 to make it closer to how the actual sensors will be together with the full robot

--- other-ideas/speed-adjustment.ino
+++ other-ideas/speed-adjustment.ino
@@ -22,7 +22,16 @@ void setup() {
 
 void loop() {
   // ReadSensors function here which will return value for linePos
-  linePos = 2*(random(-2,2));   // PLACEHOLDER: for now using random numbers for linePos
+  
+    // PLACEHOLDER: for now using random numbers for linePos
+  int zeroChance = random(0,3);  // 33.33% chance the position is 0, this could more accurately represent how our readings probably will actually be with the full robot built 
+  if (zeroChance == 0){          
+    linePos = 0;
+  }
+  else{
+    linePos = 2*(random(-2,3));  // since just using one random generator will have equal chance for each number
+  }
+
   Serial.print(String(linePos)+"  ");
 
   switch (linePos){
@@ -54,29 +63,55 @@ void loop() {
   }
 
   if (countL2 == consecutive){
-    speed = speed*0.75;  // decreases speed by 25% (maybe add minimum speed for robot, will need testing)
-    countL2 = 0; // resets counter
-    Serial.print("speed changed to: "+String(speed));
+    if (speed>30){    // only decreases speed if above lower threshold of 30
+      speed = speed*0.75;  // decreases speed by 25% 
+      countL2 = 0;  // resets counter
+      if (speed<30){  // if speed goes below 30 it is reset to 30
+        speed = 30;
+      }
+      Serial.print("speed decreased to: "+String(speed));
+    }
   } 
   else if (countL1 == consecutive){
-    speed = speed*0.9;  // decreases speed by 10%
-    countL1 = 0;
-    Serial.print("speed changed to: "+String(speed));
+    if (speed>50){     // only decreases speed if above 50
+      speed = speed*0.9;  // decreases speed by 10%
+      countL1 = 0;
+      if (speed<50){  // if speed goes below 50 it is reset to 50
+        speed = 50;
+      }
+      Serial.print("speed decreased to: "+String(speed));
+    } 
   }
   else if (countM == consecutive){
-    speed = 150;  // resets speed to normal if robot is following a long straight line 
-    countM = 0; 
-    Serial.print("speed reset to: "+String(speed));
+    if (speed<150){   // only increases if speed below maximum threshold of 150
+      speed = speed + 25;  // increases speed if robot is following a long straight line 
+      countM = 0; 
+      if (speed>150){  // ensures speed never goes above 150
+        speed = 150;
+      }
+      Serial.print("speed increased to: "+String(speed));
+    }
+    
   }
   else if (countR1 == consecutive){
-    speed = speed*0.9;
-    countR1 = 0;
-    Serial.print("speed changed to: "+String(speed));
+    if (speed>50){
+      speed = speed*0.9;
+      countR1 = 0;
+      if (speed<50){
+        speed = 50;
+      }
+      Serial.print("speed decreased to: "+String(speed));
+    }
   }
   else if (countR2 == consecutive){
-    speed = speed*0.75;
-    countR2 = 0;
-    Serial.print("speed changed to: "+String(speed));
+    if (speed>30){
+      speed = speed*0.75;
+      countR2 = 0;
+      if (speed<30){
+        speed = 30;
+      }
+      Serial.print("speed decreased to: "+String(speed));
+    }
   }
 
   Serial.println(); // goes to next line
