039067e65de465c24aebe38348e17957fda66085
Mustafa0801 <mustafahassancheema@gmail.com>
2026-09-20 21:05:52 +0500

    edit to have seperate speeds for both motors

--- motors-test-code/motor-test.ino
+++ motors-test-code/motor-test.ino
@@ -5,7 +5,8 @@
 #define rf 7 // front-right wheel
 #define rb 6 // back-right wheel
 
-int speed = 150; // placeholder value for speed (maybe make it controllable instead of hard coded later on)
+int leftSpeed = 150; // placeholder value for speeds 
+int rightSpeed = 150;
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,37 +25,37 @@ void loop() {
   // i.e car moves in certain direction then stops, then moves
 
   delay(1000);
-  forward(speed);  // car moves forward after 1 second delay
+  forward(leftSpeed, rightSpeed);  // car moves forward after 1 second delay
 
   delay(1500);
   stop();   // 1.5 second later car stops
 
   delay(1000);
-  right(speed); // 1 second later car moves right
+  right(leftSpeed, rightSpeed); // 1 second later car moves right
 
   delay(1000);
   stop(); 
 
   delay(1000);
-  rightTurn(speed); // car turns right
+  rightTurn(leftSpeed, rightSpeed); // car turns right
 
   delay(1000);
   stop();
   
   delay(1000);
-  reverse(speed);  //  car reverses
+  reverse(leftSpeed, rightSpeed);  //  car reverses
 
   delay(1500);
   stop(); // 1.5 second later car stops again
 
   delay(1000);
-  left(speed);  // car moves left
+  left(leftSpeed, rightSpeed);  // car moves left
 
   delay(1000);
   stop();  
 
   delay(1000);
-  leftTurn(speed);  // car turns left
+  leftTurn(leftSpeed, rightSpeed);  // car turns left
 
   delay(1000);
   stop();
