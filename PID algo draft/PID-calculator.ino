ee8e09b316f56da92379583362f7e91dd89456c0
Mustafa0801 <mustafahassancheema@gmail.com>
2026-09-20 20:26:26 +0500

    Add comments

--- PID algo draft/PID-calculator.ino
+++ PID algo draft/PID-calculator.ino
@@ -1,9 +1,9 @@
 //  FUNCTION TO FIRST CALCULATE DELTA TIME
 
 float Calculate_dt(){
-  _currentTime = millis();
-  float _dt = (_currentTime - _lastTime) / 1000.0;
-  _lastTime = _currentTime;
+  _currentTime = millis();     // total time that has passed since start
+  float _dt = (_currentTime - _lastTime) / 1000.0;    // total time minus time the previous total time before this iteration, and then divided by 1000 to convert to seconds
+  _lastTime = _currentTime;    // previous total time updated for next iteration
   Serial.print("dt is: "+String(_dt));
   if (_dt == 0){
     _dt = 0.1;    // sets a value for delta time if it is calculated as zero, since the first time loop runs dt will be zero
@@ -15,12 +15,12 @@ float Calculate_dt(){
 
 float CalculatePID(){
   float dt = Calculate_dt();
-  float _integral = _integral + error*dt;
-  float _derivative = (error - previousError) / dt;
+  float _integral = _integral + error*dt;   // sums errors over time
+  float _derivative = (error - previousError) / dt;   // rate of change of error
   
-  previousError = error;
+  previousError = error;  // previousError updated for next iteration
  
-  float _output = (Kp * error) + (Ki * _integral) + (Kd * _derivative);
+  float _output = (Kp * error) + (Ki * _integral) + (Kd * _derivative);   // PID output calculated, variables Kp, Ki, Kd will be finetuned after testing
   Serial.print(" | PID output is: "+String(_output));
   return _output;
 }
