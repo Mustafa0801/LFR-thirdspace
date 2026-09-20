4cbeb511dfbabb3e409a2bd4e13c2334ded819d3
Mustafa0801 <mustafahassancheema@gmail.com>
2026-09-20 15:50:25 +0500

    Add pid algo files

---
+++ PID-algo-sketch.ino
@@ -0,0 +1,32 @@
+//   THIS IS FIRST SKETCH FOR PID ALGORITHM
+//   IT IMPROVES UPON PREVIOUS SENSOR READING ALGORITHMS AND THE SPEED ADJUSTMENT ALGORITHM
+//   THIS IS NOT FINAL, BEING MADE WITHOUT THE ACTUAL ROBOT COMPONENTS CURRENTLY
+//   AFTER TESTING WITH BUILT ROBOT THIS CAN BE IMPROVED AND FINALIZED
+
+#define irL2 8
+#define irL1 9
+#define irM 10    // pins of sensors, same as before
+#define irR1 11
+#define irR2 12
+
+int baseSpeed = 150;
+int leftSpeed;
+int rightSpeed;
+
+float error;
+
+
+void setup() {
+  pinMode(irL2, INPUT);
+  pinMode(irL1, INPUT);
+  pinMode(irM, INPUT);
+  pinMode(irR1, INPUT);
+  pinMode(irR2, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ sensor-reading.ino
@@ -0,0 +1,28 @@
+//  SIMILAR TO ORIGINAL SENSOR READING HOWEVER IT CALCULATES ERROR DIFFERENTLY
+
+// NOTE: for now digital sensors being used but analog sensors could improve the smoothness of robot
+
+float ReadSensors(){
+  int readingL2 = digitalRead(irL2);    // seperate variable for reading for each sensor and value is printed same as before
+  Serial.print(String(readingL2)+" ");
+
+  int readingL1 = digitalRead(irL1);
+  Serial.print(String(readingL1)+" ");
+
+  int readingM = digitalRead(irM);
+  Serial.print(String(readingM)+" ");
+
+  int readingR1 = digitalRead(irR1);
+  Serial.print(String(irR1)+" ");
+
+  int readingR2 = digitalRead(irR2);
+  Serial.print(String(irR2));
+
+  // the algorithm calculates error using weighted position algorithm
+  // e.g if robot is on the line then readings will be "1 1 0 1 1" , _robotPos=2.0 so _error=0
+
+  float _robotPos = (readingL2*0 + readingL1*1 + readingM*2 + readingR1*3 + readingR2*4) / (readingL2 + readingL1 + readingM + readingR1 + readingR2);
+  float _normalPos = 2.0;
+  float _error = _robotPos - _normalPos;
+  return _error;
+}
