bd4bb2f0fac5d19dca0d95bff61d972988dd7994
Mustafa0801 <mustafahassancheema@gmail.com>
2026-09-24 20:12:50 +0500

    edit functions to have seperate speed for both motors

--- Arduino Code/fullcode-draft1/move-funcs.ino
+++ Arduino Code/fullcode-draft1/move-funcs.ino
@@ -10,9 +10,9 @@ void stop(){
   Serial.print(" | stopped ");
 }
 
-void forward(int _speed){
-  analogWrite(motorLeft, _speed);
-  analogWrite(motorRight, _speed);
+void forward(int _speedL, int _speedR){
+  analogWrite(motorLeft, _speedL);
+  analogWrite(motorRight, _speedR);
   digitalWrite(lf, HIGH);
   digitalWrite(lb, LOW);
   digitalWrite(rf, HIGH);
@@ -20,8 +20,8 @@ void forward(int _speed){
   Serial.print(" | forward ");
 }
 
-void right(int _speed){
-  analogWrite(motorLeft, _speed);
+void right(int _speedL, int _speedR){
+  analogWrite(motorLeft, _speedL);
   digitalWrite(motorRight, LOW);
   digitalWrite(lf, HIGH);
   digitalWrite(lb, LOW);
@@ -30,9 +30,9 @@ void right(int _speed){
   Serial.print(" | right ");
 }
 
-void rightTurn(int _speed){
-  analogWrite(motorLeft, _speed);
-  analogWrite(motorRight, _speed);
+void rightTurn(int _speedL, int _speedR){
+  analogWrite(motorLeft, _speedL);
+  analogWrite(motorRight, _speedR);
   digitalWrite(lf, HIGH);
   digitalWrite(lb, LOW);
   digitalWrite(rf, LOW);
@@ -40,8 +40,8 @@ void rightTurn(int _speed){
   Serial.print(" | right point turn ");
 }
 
-void left(int _speed){
-  analogWrite(motorLeft, _speed);
+void left(int _speedL, int _speedR){
+  analogWrite(motorLeft, _speedL);
   digitalWrite(motorRight, LOW);
   digitalWrite(lf, LOW);
   digitalWrite(lb, HIGH);
@@ -50,15 +50,13 @@ void left(int _speed){
   Serial.print(" | left ");
 }
 
-void leftTurn(int _speed){
-  analogWrite(motorLeft, _speed);
-  analogWrite(motorRight, _speed);
+void leftTurn(int _speedL, int _speedR){
+  analogWrite(motorLeft, _speedL);
+  analogWrite(motorRight, _speedR);
   digitalWrite(lf, LOW);
   digitalWrite(lb, HIGH);
   digitalWrite(rf, HIGH);
   digitalWrite(rb, LOW);
   Serial.print(" | left point turn ");
 }
-
-
 
